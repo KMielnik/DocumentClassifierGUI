30aa51837fd029e99b3ca292e5294e395e4a4882
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2019-11-29 17:03:53 +0100

    Start of the experiment with drawing polygons

--- DocumentClassifierGUI/DocumentMarkingView.xaml.cs
+++ DocumentClassifierGUI/DocumentMarkingView.xaml.cs
@@ -18,9 +18,39 @@ namespace DocumentClassifierGUI
     /// </summary>
     public partial class DocumentMarkingView : UserControl, IDocumentMarkingView
     {
+        private Polygon actualPolygon;
+        private Point currentPoint;
         public DocumentMarkingView()
         {
             InitializeComponent();
+
+            resetActualPolygon();
+
+            DocumentSurface.Background = new ImageBrush(new BitmapImage(new Uri(@"test_document.jpg", UriKind.Relative))) { Stretch = Stretch.Fill };
+            DocumentSurface.Children.Add(actualPolygon);
+        }
+
+        private void resetActualPolygon()
+        {
+            actualPolygon = new Polygon()
+            {
+                Stroke = Brushes.Black,
+                Fill = Brushes.Red,
+                FillRule = FillRule.Nonzero,
+                Opacity = 0.6
+            };
+        }
+
+        private void DocumentSurface_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                actualPolygon.Points.Add(e.GetPosition(this));
+        }
+
+        private void DocumentSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                actualPolygon.Points.Add(e.GetPosition(this));
         }
     }
 }
