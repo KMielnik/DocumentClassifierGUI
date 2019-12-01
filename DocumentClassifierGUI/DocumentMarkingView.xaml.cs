b0ea22f73a92b43333901ac1ad160763145c1c41
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2019-12-01 19:26:39 +0100

    Points around polygon + undo

--- DocumentClassifierGUI/DocumentMarkingView.xaml.cs
+++ DocumentClassifierGUI/DocumentMarkingView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,7 +22,8 @@ namespace DocumentClassifierGUI
     public partial class DocumentMarkingView : UserControl, IDocumentMarkingView
     {
         private Polygon actualPolygon;
-        private Point currentPoint;
+        private ObservableCollection<Point> actualPolygonCheckpoints = new ObservableCollection<Point>();
+        private List<Ellipse> points = new List<Ellipse>();
         public DocumentMarkingView()
         {
             InitializeComponent();
@@ -28,6 +32,35 @@ namespace DocumentClassifierGUI
 
             DocumentSurface.Background = new ImageBrush(new BitmapImage(new Uri(@"test_document.jpg", UriKind.Relative))) { Stretch = Stretch.Fill };
             DocumentSurface.Children.Add(actualPolygon);
+
+            actualPolygonCheckpoints.CollectionChanged += ActualPolygonCheckpoints_CollectionChanged;
+        }
+
+        private void ActualPolygonCheckpoints_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            foreach (var point in points)
+                DocumentSurface.Children.Remove(point);
+
+            points.Clear();
+
+            foreach (var point in actualPolygonCheckpoints)
+            {
+                var elipse = new Ellipse()
+                {
+                    Width = 6,
+                    Height = 6,
+                    Stroke = Brushes.Blue,
+                    Fill = Brushes.Red,
+                    StrokeThickness = 3
+                };
+
+                points.Add(elipse);
+
+                DocumentSurface.Children.Add(elipse);
+
+                Canvas.SetLeft(elipse, point.X - elipse.Width / 2);
+                Canvas.SetTop(elipse, point.Y - elipse.Height / 2);
+            }
         }
 
         private void resetActualPolygon()
@@ -39,12 +72,32 @@ namespace DocumentClassifierGUI
                 FillRule = FillRule.Nonzero,
                 Opacity = 0.6
             };
+
+            actualPolygonCheckpoints.Clear();
         }
 
         private void DocumentSurface_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ButtonState == MouseButtonState.Pressed)
-                actualPolygon.Points.Add(e.GetPosition(this));
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var newPoint = e.GetPosition(this);
+
+                actualPolygon.Points.Add(newPoint);
+                actualPolygonCheckpoints.Add(newPoint);
+            }
+            else if(e.RightButton == MouseButtonState.Pressed)
+            {
+                if (actualPolygonCheckpoints.Count != 0)
+                {
+                    var lastPoint = actualPolygonCheckpoints.Last();
+                    actualPolygonCheckpoints.Remove(lastPoint);
+
+                    var index = actualPolygon.Points.IndexOf(lastPoint);
+
+                    for (int i = actualPolygon.Points.Count - 1; i >= index; i--)
+                        actualPolygon.Points.RemoveAt(index);
+                }
+            }
         }
 
         private void DocumentSurface_MouseMove(object sender, MouseEventArgs e)
