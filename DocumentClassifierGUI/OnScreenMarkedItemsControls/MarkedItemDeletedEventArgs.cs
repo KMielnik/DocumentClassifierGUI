4f15f02f33654598a481bc7e144773b877ef0770
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2020-01-25 22:04:12 +0100

    Few comments in code for documentations ->OnScreenMArkedItemsControls folder.

--- DocumentClassifierGUI/OnScreenMarkedItemsControls/MarkedItemDeletedEventArgs.cs
+++ DocumentClassifierGUI/OnScreenMarkedItemsControls/MarkedItemDeletedEventArgs.cs
@@ -2,10 +2,20 @@
 
 namespace DocumentClassifierGUI.OnScreenMarkedItemsControls
 {
+    /// <summary>
+    /// Argumenty zdarzenia wywolywanego podczas usuwania oznaczonego elementu z listy.
+    /// </summary>
     public class MarkedItemDeletedEventArgs : EventArgs
     {
+        /// <summary>
+        /// Element do usuniecia.
+        /// </summary>
         public MarkedItem deletedItem;
 
+        /// <summary>
+        /// Konstruktor klasy.
+        /// </summary>
+        /// <param name="markedItem">Element do usuniecia</param>
         public MarkedItemDeletedEventArgs(MarkedItem markedItem)
         {
             deletedItem = markedItem;
--- DocumentClassifierGUI/OnScreenMarkedItemsControls/OnScreenMarkedItemsView.xaml.cs
+++ DocumentClassifierGUI/OnScreenMarkedItemsControls/OnScreenMarkedItemsView.xaml.cs
@@ -8,24 +8,40 @@ using System.Windows.Input;
 namespace DocumentClassifierGUI.OnScreenMarkedItemsControls
 {
     /// <summary>
-    /// Interaction logic for OnScreenMarkedItemsView.xaml
+    /// Logika interakcji dla OnScreenMarkedItemsView.xaml, pozwalajacego na zarzadzanie lista oznaczen na aktualnym dokumencie.
     /// </summary>
     public partial class OnScreenMarkedItemsView : UserControl, IOnScreenMarkedItemsView
     {
+        /// <summary>
+        /// Lista oznaczen.
+        /// </summary>
         public ObservableCollection<MarkedItem> MarkedItems { get; set; } = new ObservableCollection<MarkedItem>();
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
         public OnScreenMarkedItemsView()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Zdarzenie usuniecia oznaczenia z listy.
+        /// </summary>
         public event EventHandler<MarkedItemDeletedEventArgs> MarkedItemDeleted;
 
+        /// <summary>
+        /// Ustawienie na liscie aktualnych oznaczen dokumentu.
+        /// </summary>
+        /// <param name="markedItems">Oznaczenia do umieszczenia na liscie.</param>
         public void SetMarkedItems(List<MarkedItem> markedItems)
         {
             MarkedItems.Clear();
             markedItems.ForEach(x => MarkedItems.Add(x));
         }
 
+        /// <summary>
+        /// Proboje usunac dany oznaczony element z dokumentu.
+        /// </summary>
         private void TryToDeleteSelectedItem()
         {
             var selectedItem = MarkedItemsListBox.SelectedItem;
@@ -34,12 +50,22 @@ namespace DocumentClassifierGUI.OnScreenMarkedItemsControls
                 MarkedItemDeleted?.Invoke(this, new MarkedItemDeletedEventArgs(MarkedItems[MarkedItemsListBox.Items.IndexOf(selectedItem)]));
         }
 
+        /// <summary>
+        /// Obsluguje usuniecie obiektu przy pomocy klawisza delete.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Grid_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Delete)
                 TryToDeleteSelectedItem();
         }
 
+        /// <summary>
+        /// Obsluguje usuniecie obiektu przy pomocy przycisku w GUI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             TryToDeleteSelectedItem();
